427e077dbda7de7d740b01aa802362dddc2c401f
arietwigt <twigt.arie@gmail.com>
2022-08-08 14:05:15 +0200

    added example for extracting data from the database through the argparser

--- custom_modules/car_functions.py
+++ custom_modules/car_functions.py
@@ -1,8 +1,14 @@
 import requests
 import pandas as pd
 import os
+from sqlalchemy import create_engine
 
-def import_cars_by_brand(brand: str, selected_color: str='ZWART', export_to_csv=False) -> list:
+
+def import_cars_by_brand(brand: str, 
+                         selected_color: str='ZWART', 
+                         export_to_csv=False,
+                         export_to_db=False,
+                         from_db=False) -> list:
     '''
     Returns a list of cars by the specified brand
     
@@ -10,6 +16,15 @@ def import_cars_by_brand(brand: str, selected_color: str='ZWART', export_to_csv=
     * brand, a car brand
     * colour
     '''
+    
+    db_name = "data.db"
+    con = create_engine(f'sqlite:///{db_name}')
+    
+    if from_db:
+        qry = "select * from cars where merk = ? limit 5"
+        imported_cars_df = pd.read_sql_query(qry, con=con, params=(brand,))
+        return imported_cars_df
+    
     
     endpoint = f"https://opendata.rdw.nl/resource/m9d7-ebf2.json?merk={brand}"
     response = requests.get(endpoint)
@@ -22,8 +37,10 @@ def import_cars_by_brand(brand: str, selected_color: str='ZWART', export_to_csv=
                           for car in cars_list
                           if car['eerste_kleur'] == selected_color]
     
+    # convert the cars list to a data frame
     cars_df = pd.DataFrame(filtered_cars_list)
     
+    # option for export to csv
     if export_to_csv:
         folder_name = f"cars_data/{brand}"
         os.makedirs(folder_name, exist_ok=True)
@@ -32,10 +49,15 @@ def import_cars_by_brand(brand: str, selected_color: str='ZWART', export_to_csv=
         cars_df.to_csv(filename, 
                    sep=";",
                    index=None)
-    else:
-        print(cars_df)
     
-    return cars_df
+    
+    # option for export do database
+    if export_to_db:
+        print("\nExporting to database")
+        
+        cars_df.to_sql("cars", con=con, if_exists="append", index=False)
+        print("Succesfully exported to database")
+        return cars_df
     
     
   
--- main_args.py
+++ main_args.py
@@ -9,6 +9,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--brand', required=True, type=str)
 parser.add_argument('--colour', required=False, type=str, default='WIT')
 parser.add_argument('--csv_export', required=False, type=bool, default=False)
+parser.add_argument('--db_export', required=False, type=bool, default=False)
+parser.add_argument('--from_db', required=False, type=bool, default=False)
 
 # parse the arguments
 args = parser.parse_args()
@@ -19,6 +21,16 @@ if __name__ ==  '__main__':
     selected_brand = args.brand
     selected_colour = args.colour
     export_to_csv = args.csv_export
+    export_to_db = args.db_export
+    from_db = args.from_db
     
     # execute the function
-    import_cars_by_brand(selected_brand, selected_colour, export_to_csv=export_to_csv)
+    df = import_cars_by_brand(selected_brand, 
+                         selected_colour, 
+                         export_to_csv=export_to_csv,
+                         export_to_db=export_to_db,
+                         from_db=from_db
+                         )
+    
+    # print out the cars
+    print(df)
